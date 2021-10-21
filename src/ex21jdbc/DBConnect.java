fbde4c8442167a346dc3bdb9f8b8dcf4ff392837
sonjy2717 <sonjy2717@naver.com>
2021-10-21 18:08:01 +0900

    성공!

--- src/ex21jdbc/DBConnect.java
+++ src/ex21jdbc/DBConnect.java
@@ -8,11 +8,27 @@ public class DBConnect {
 	public static void main(String[] args) {
 		
 		try {
+			/*
+			JDBC프로그래밍 절차
+			
+			1. 오라클용 JDBC 드라이버를 메모리에 로드
+			: new를 사용하지 않고 클래스명으로 직접 찾아서 객체를 생성한 후
+			메모리에 로드하는 forName() 메서드를 사용한다. 메모리에 로드된
+			드라이버가 DriverManager라는 클래스에 등록된다.
+			 */
 			Class.forName("oracle.jdbc.OracleDriver");
+			/*
+			2. 오라클 연결을 위한 커넥션URL, 계정아이디, 패스워드를 준비한다.
+			jdbc:oracle:thin:@오라클서버의IP주소:포트번호:SID명
+			※ 서버 환경에 따라 ip주소, 포트번호, sid는 변경될 수 있다.
+			 */
 			String url = "jdbc:oracle:thin:@localhost:1521:xe";
 			String id = "hr";
 			String pass = "1234";
 			
+			/*
+			3. DriverManager 클래스의 메서드를 통해 오라클 연결
+			 */
 			Connection con = DriverManager.getConnection(url, id, pass);
 			if (con != null) {
 				System.out.println("Oracle 연결 성공");
